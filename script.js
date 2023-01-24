c08762e5cc932fb071b3b980947e9bf4d7ebb045
Muadz <muadzyamani@gmail.com>
2023-01-24 09:54:06 +0800

    Try different code structure

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const form = document.querySelector('.add-book-form');
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const addBookBtn = document.querySelector('.input.button');
-
+// Data Structures
 
 class Book {
     constructor(title, author, pages, isRead) {
@@ -19,57 +14,81 @@ class Library {
         this.books = [];
     }
 
-    start() {
-        this.handleInput();
+    addBook(newBook) {
+        this.books.push(newBook);
     }
 
-    handleInput() {
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            const book = new Book(title.value, author.value, pages.value, false);
-            
-            this.addBook(book);
-            this.updateLibrary();
-            this.clearInputBoxes();
-        });
-    }
-
-    addBook(book) {
-        this.books.push(book);
-    }
-
-    updateLibrary() {
-        let libraryContainer = document.querySelector('.library-container')
-        let bookContainer = document.createElement('div');
-        bookContainer.classList.add('book-container');
-
-        // Use template literals to create the inner HTML
-        bookContainer.innerHTML = `
-            <div class="book-details-container">
-                <div class="book-title">"${title.value}"</div>
-                <div class="book-author">${author.value}</div>
-                <div class="book-pages">${pages.value} pages</div>
-            </div>
-            <div class="book-actions-container">
-                <button class="action-button read-button">Not Read</button>
-                <button class="action-button remove-button">Remove</button>
-            </div>
-        `;
-
-        // Append the top level element to the Library Container
-        libraryContainer.appendChild(bookContainer);
-    }
-
-    removeBook(book) {
-        this.books = this.books.filter(b => b !== book);
-    }
-
-    clearInputBoxes() {
-        title.value = '';
-        author.value = '';
-        pages.value = '';
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title);
     }
 }
 
 const library = new Library();
-library.start();
+
+
+// User Interface
+
+const libraryContainer = document.querySelector('.library-container');
+const addBookForm = document.querySelector('.add-book-form');
+const addBookBtn = document.querySelector('.input.button');
+
+
+const updateBooksContainer = () => {
+    resetBooksContainer();
+    for (let book of library.books) {
+        createBookContainer(book);
+    }
+}
+
+const resetBooksContainer = () => {
+    libraryContainer.innerHTML = '';
+}
+
+const createBookContainer = (book) => {
+    const bookContainer = document.createElement('div');
+    bookContainer.classList.add('book-container');
+
+    bookContainer.innerHTML = `
+        <div class="book-details-container">
+            <div class="book-title">"${book.title}"</div>
+            <div class="book-author">${book.author}</div>
+            <div class="book-pages">${book.pages} pages</div>
+        </div>
+        <div class="book-actions-container">
+            <button class="action-button read-button">Not Read</button>
+            <button class="action-button remove-button">Remove</button>
+        </div>
+    `;
+
+    libraryContainer.appendChild(bookContainer);
+
+    const removeBtn = document.querySelector('.remove-button');
+    removeBtn.onclick = removeBook;
+}
+
+const getBookFromInput = () => {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+
+    return new Book(title, author, pages, false);
+}
+
+const addBook = (event) => {
+    event.preventDefault();
+    const newBook = getBookFromInput();
+
+    library.addBook(newBook);
+    updateBooksContainer();
+}
+
+const removeBook = (event) => {
+    const title = event.target.parentNode.previousElementSibling.querySelector('.book-title').innerHTML.replaceAll('"', '');
+
+    console.log(event.target.parentNode.previousElementSibling.querySelector('.book-title').innerHTML);
+
+    library.removeBook(title);
+    updateBooksContainer();
+}
+
+addBookForm.onsubmit = addBook;
